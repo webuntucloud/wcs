bef321c0bd06728b2ad139b4c7f4a053f3704883
Andrew Michaels <83120175+andrewstanleymichaels@users.noreply.github.com>
2022-04-10 19:44:22 +0200

    Update variables.js

--- src/styles/variables.js
+++ src/styles/variables.js
@@ -2,10 +2,10 @@ import { css } from 'styled-components';
 
 const variables = css`
   :root {
-    --dark-navy: #020c1b;
-    --navy: #0a192f;
-    --light-navy: #112240;
-    --lightest-navy: #233554;
+    --dark-navy: #0D2430;
+    --navy: #2C6D7B;
+    --light-navy: #153B50;
+    --lightest-navy: #429EA6;
     --navy-shadow: rgba(2, 12, 27, 0.7);
     --dark-slate: #495670;
     --slate: #8892b0;
