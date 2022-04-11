a7424f52e1758b291f0594d59d4829800a1ca032
Andrew Michaels <83120175+andrewstanleymichaels@users.noreply.github.com>
2022-04-12 01:47:21 +0200

    Update contact.js

--- src/components/sections/contact.js
+++ src/components/sections/contact.js
@@ -64,7 +64,7 @@ const Contact = () => {
         Whether you have a question or just want to say hi, I’ll try my best to get back to you!
       </p>
 
-      <a className="email-link" href={`mailto:info@andrewmichaels.co.za`}>
+      <a className="email-link" href={`mailto:${email}`}>
         Say Hello
       </a>
     </StyledContactSection>
