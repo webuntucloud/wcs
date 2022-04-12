a49da23658d2cf01a919d65a5207d6b9436b5c51
Andrew Michaels <83120175+andrewstanleymichaels@users.noreply.github.com>
2022-04-12 14:22:33 +0200

    Update mixins.js

--- src/styles/mixins.js
+++ src/styles/mixins.js
@@ -133,15 +133,7 @@ const mixins = {
     }
   `,
 
-  boxShadow: css`
-    box-shadow: 0 10px 30px -15px var(--navy-shadow);
-    transition: var(--transition);
 
-    &:hover,
-    &:focus {
-      box-shadow: 0 20px 30px -15px var(--navy-shadow);
-    }
-  `,
 
   fancyList: css`
     padding: 0;
