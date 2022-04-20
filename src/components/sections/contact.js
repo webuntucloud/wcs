4ad5c31dd433408040adeff1f8aa62e2f4ec3738
Andrew Michaels <83120175+andrewstanleymichaels@users.noreply.github.com>
2022-04-21 00:37:33 +0200

    Update contact.js

--- src/components/sections/contact.js
+++ src/components/sections/contact.js
@@ -61,7 +61,7 @@ const Contact = () => {
 
       <p>
         For any new opportunities, my inbox is always open.
-        Whether you have a question or just want to say hi, I’ll try my best to get back to you!
+        Whether you have a project for us or just want to say hi, I’ll try my best to get back to you!
       </p>
 
       <a className="email-link" href={`mailto:${email}`}>
