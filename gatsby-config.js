9c5db9b4ba777208e0a945a95d805d1af7b7ac25
Andrew Michaels <83120175+andrewstanleymichaels@users.noreply.github.com>
2022-04-21 02:33:36 +0200

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,8 +20,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'BrittanyChiang',
-        short_name: 'BrittanyChiang',
+        name: 'Andrew Michaels',
+        short_name: 'Andrew Michaels',
         start_url: '/',
         background_color: config.colors.darkNavy,
         theme_color: config.colors.navy,
