234998c399762351a5136ecf34adf45064592365
Andrew Michaels <83120175+dremichaels@users.noreply.github.com>
2022-11-05 08:34:17 +0200

    Update featured.js

--- src/components/sections/featured.js
+++ src/components/sections/featured.js
@@ -297,7 +297,7 @@ const StyledProject = styled.li`
         object-fit: cover;
         width: auto;
         height: 100%;
-        filter: grayscale(100%) contrast(1) brightness(50%);
+        filter: none;
       }
     }
   }
